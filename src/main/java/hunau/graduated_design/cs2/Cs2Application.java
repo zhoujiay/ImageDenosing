f4f278b8177f8b170ce69665274718bd8d037ee5
JY Zhou <jy.zterch@foxmail.com>
2023-05-16 20:46:33 +0800

    Initial commit

---
+++ src/main/java/hunau/graduated_design/cs2/Cs2Application.java
@@ -0,0 +1,13 @@
+package hunau.graduated_design.cs2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs2Application.class, args);
+    }
+
+}
---
+++ src/test/java/hunau/graduated_design/cs2/Cs2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hunau.graduated_design.cs2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
